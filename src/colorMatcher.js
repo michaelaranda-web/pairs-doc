48a12c2d5b420e57087cbcfd3af4cf7046e3aafe
Michael Aranda <michaelaranda0.web@gmail.com>
2017-03-12 14:37:24 -0700

    Add more color palettes

--- src/colorMatcher.js
+++ src/colorMatcher.js
@@ -33,16 +33,25 @@ function getRandomColorPalette() {
 }
 
 function getColorPalettes() {
+  //From Adobe Color Wheel https://color.adobe.com/explore/?filter=most-popular&time=all
+  
   return [
-    ["#3E3947", "#735360", "#D68684", "#F1B0B0", "#EBD0C4"],
-    ["#225378", "#1695A3", "#ACF0F2", "#F3FFE2", "#EB7F00"],
-    ["#595241", "#B8AE9C", "#FFFFFF", "#ACCFCC", "#8A0917"],
-    ["#04BFBF", "#CAFCD8", "#F7E967", "#A9CF54", "#588F27"],
-    ["#D8CAA8", "#5C832F", "#284907", "#382513", "#363942"],
-    ["#000000", "#263248", "#7E8AA2", "#FFFFFF", "#FF9800"],
-    ["#BA2F1D", "#FFF8A4", "#F5E67F", "#264A59", "#1E2C30"],
-    ["#2F3837", "#C5C7B6", "#FFF8D3", "#4C493E", "#222028"],
-    ["#E0FFB3", "#61C791", "#31797D", "#2A2F36", "#F23C55"]
+    ["#3E3947", "#735360", "#D68684", "#F1B0B0", "#EBD0C4"], //Graceful
+    ["#225378", "#1695A3", "#ACF0F2", "#F3FFE2", "#EB7F00"], //Aspirin-C
+    ["#595241", "#B8AE9C", "#FFFFFF", "#ACCFCC", "#8A0917"], //Tech Office
+    ["#04BFBF", "#CAFCD8", "#F7E967", "#A9CF54", "#588F27"], //Pear Lemon Fizz
+    ["#000000", "#263248", "#7E8AA2", "#FFFFFF", "#FF9800"], //1944Mustang
+    ["#BA2F1D", "#FFF8A4", "#F5E67F", "#264A59", "#1E2C30"], //Prep School Pendant
+    ["#2F3837", "#C5C7B6", "#FFF8D3", "#4C493E", "#222028"], //Early Winter Morning
+    ["#E6E2AF", "#A7A37E", "#EFECCA", "#046380", "#002F2F"], //sandy stone ocean beach diver
+    ["#E0FFB3", "#61C791", "#31797D", "#2A2F36", "#F23C55"], //Only Human
+    ["#468966", "#FFF0A5", "#FFB03B", "#B64926", "#8E2800"], //Firenze
+    ["#FCFFF5", "#D1DBBD", "#91AA9D", "#3E606F", "#193441"], //Neutral Blue
+    ["#FF6138", "#FFFF9D", "#BEEB9F", "#79BD8F", "#00A388"], //Phaedra 
+    ["#F6F792", "#333745", "#77C4D3", "#DAEDE2", "#EA2E49"], //CS04 
+    ["#105B63", "#FFFAD5", "#FFD34E", "#DB9E36", "#BD4932"], //Honey Pot
+    ["#FFF6C9", "#C8E8C7", "#A4DEAB", "#85CC9F", "#499E8D"], //beach glass
+    ["#C7B773", "#E3DB9A", "#F5FCD0", "#B1C2B3", "#778691"] //heirloom one 
   ];  
 }
 
